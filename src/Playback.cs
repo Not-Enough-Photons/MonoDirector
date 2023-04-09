061000ba091bc7f8930fc99358bb1c34e80387ef
adamdev <hmullinsk@hotmail.com>
2023-04-08 23:25:31 -0400

    Readded some missing scripts

---
+++ src/Playback.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections;
+using MelonLoader;
+using NEP.MonoDirector.Actors;
+using NEP.MonoDirector.State;
+
+using UnityEngine;
+
+namespace NEP.MonoDirector.Core
+{
+    public class Playback
+    {
+        public Playback()
+        {
+            if(instance == null)
+            {
+                instance = this;
+            }
+
+            Events.OnPrePlayback += OnPrePlayback;
+            Events.OnPlaybackTick += OnPlaybackTick;
+            Events.OnStopPlayback += OnStopPlayback;
+        }
+
+        public static Playback instance;
+
+        public int PlaybackTick { get => playbackTick; }
+
+        private Coroutine playRoutine;
+
+        private int playbackTick;
+
+        public void LateUpdate()
+        {
+            if (Director.PlayState != PlayState.Playing)
+            {
+                return;
+            }
+
+            Events.OnPlaybackTick?.Invoke();
+        }
+
+        public void BeginPlayback()
+        {
+            if (playRoutine == null)
+            {
+                playRoutine = MelonCoroutines.Start(PlayRoutine()) as Coroutine;
+            }
+        }
+
+        public void OnPrePlayback()
+        {
+            playbackTick = 0;
+
+            foreach (var castMember in Director.instance.Cast)
+            {
+                AnimateActor(0, castMember);
+            }
+
+            foreach (var prop in Director.instance.WorldProps)
+            {
+                if (prop == null)
+                {
+                    continue;
+                }
+                else
+                {
+                    AnimateProp(0, prop);
+                    prop.gameObject.SetActive(true);
+                }
+            }
+        }
+
+        public void OnPlaybackTick()
+        {
+            if (Director.PlayState == PlayState.Paused)
+            {
+                return;
+            }
+
+            AnimateAll(playbackTick);
+
+            playbackTick++;
+        }
+
+        public void OnStopPlayback()
+        {
+            if (playRoutine != null)
+            {
+                MelonCoroutines.Stop(playRoutine);
+                playRoutine = null;
+            }
+        }
+
+        public void Seek(int rate)
+        {
+            if(Director.PlayState != PlayState.Stopped)
+            {
+                return;
+            }
+
+            if(playbackTick <= 0)
+            {
+                playbackTick = 0;
+            }
+
+            if(playbackTick > Recorder.instance.RecordTick)
+            {
+                playbackTick = Recorder.instance.RecordTick;
+            }
+
+            AnimateAll(playbackTick);
+            playbackTick += rate;
+        }
+
+        public void AnimateAll(int frame)
+        {
+            foreach (var castMember in Director.instance.Cast)
+            {
+                AnimateActor(frame, castMember);
+            }
+
+            foreach (var prop in Director.instance.WorldProps)
+            {
+                AnimateProp(frame, prop);
+            }
+        }
+
+        public void AnimateActor(int frame, Actor actor)
+        {
+            if(actor == null)
+            {
+                return;
+            }
+
+            actor.Act(frame);
+        }
+
+        public void AnimateNPC(int frame, ActorNPC actor)
+        {
+            if(actor == null)
+            {
+                return;
+            }
+
+            actor.Act(frame);
+        }
+
+        public void AnimateProp(int frame, ActorProp prop)
+        {
+            if(prop == null)
+            {
+                return;
+            }
+
+            prop.Act(frame);
+        }
+
+        public IEnumerator PlayRoutine()
+        {
+            Events.OnPrePlayback?.Invoke();
+            yield return new WaitForSeconds(4f);
+            Events.OnPlay?.Invoke();
+
+            while (Director.PlayState == PlayState.Playing || Director.PlayState == PlayState.Paused)
+            {
+                yield return null;
+            }
+
+            Events.OnStopPlayback?.Invoke();
+            yield return null;
+        }
+    }
+}
---
+++ src/Recorder.cs
@@ -0,0 +1,157 @@
+﻿using System.Collections;
+using MelonLoader;
+using NEP.MonoDirector.Actors;
+using NEP.MonoDirector.State;
+
+using UnityEngine;
+
+namespace NEP.MonoDirector.Core
+{
+    public class Recorder
+    {
+        public Recorder()
+        {
+            if (instance == null)
+            {
+                instance = this;
+            }
+
+            Events.OnPreRecord += OnPreRecord;
+            Events.OnRecordTick += OnRecordTick;
+            Events.OnStopRecording += OnStopRecording;
+        }
+
+        public static Recorder instance;
+
+        public int RecordTick { get => recordTick; }
+
+        public ActorPlayer ActiveActor { get => activeActor; }
+
+        private ActorPlayer activeActor;
+
+        private Coroutine recordRoutine;
+
+        private int recordTick;
+
+        public void LateUpdate()
+        {
+            if (Director.PlayState != PlayState.Recording)
+            {
+                return;
+            }
+
+            Events.OnRecordTick?.Invoke();
+        }
+
+        public void BeginRecording()
+        {
+            if (recordRoutine == null)
+            {
+                recordRoutine = MelonCoroutines.Start(RecordRoutine()) as Coroutine;
+            }
+        }
+
+        public void RecordCamera()
+        {
+            foreach (var castMember in Director.instance.Cast)
+            {
+                castMember?.Act(Director.instance.WorldTick);
+            }
+        }
+
+        public void RecordActor()
+        {
+            activeActor.RecordFrame();
+
+            foreach (var prop in Director.instance.RecordingProps)
+            {
+                prop.Record(recordTick);
+            }
+
+            foreach (var castMember in Director.instance.Cast)
+            {
+                Playback.instance.AnimateActor(recordTick, castMember);
+            }
+
+            foreach(var npcCastMember in Director.instance.NPCCast)
+            {
+                Playback.instance.AnimateNPC(recordTick, npcCastMember);
+            }
+
+            foreach(var prop in Director.instance.WorldProps)
+            {
+                Playback.instance.AnimateProp(recordTick, prop);
+            }
+        }
+
+        public void OnPreRecord()
+        {
+            if (recordTick > 0)
+            {
+                recordTick = 0;
+            }
+
+            activeActor = new ActorPlayer(Constants.rigManager.avatar);
+
+            foreach (var castMember in Director.instance.Cast)
+            {
+                if (castMember != null)
+                {
+                    castMember.Act(0);
+                }
+            }
+        }
+
+        public void OnRecordTick()
+        {
+            if (Director.PlayState == PlayState.Paused)
+            {
+                return;
+            }
+
+            recordTick++;
+
+            if (Director.CaptureState == CaptureState.CaptureCamera)
+            {
+                RecordCamera();
+            }
+
+            if (Director.CaptureState == CaptureState.CaptureActor)
+            {
+                RecordActor();
+            }
+        }
+
+        public void OnStopRecording()
+        {
+            activeActor.CloneAvatar();
+            Director.instance.Cast.Add(activeActor);
+
+            activeActor = null;
+
+            Director.instance.WorldProps.AddRange(Director.instance.RecordingProps);
+            Director.instance.RecordingProps.Clear();
+
+            if (recordRoutine != null)
+            {
+                MelonCoroutines.Stop(recordRoutine);
+                recordRoutine = null;
+            }
+        }
+
+        public IEnumerator RecordRoutine()
+        {
+            Events.OnPreRecord?.Invoke();
+            yield return new WaitForSeconds(5f);
+            Events.OnStartRecording?.Invoke();
+
+            while (Director.PlayState == PlayState.Recording || Director.PlayState == PlayState.Paused)
+            {
+                yield return null;
+            }
+
+            Events.OnStopRecording?.Invoke();
+            yield return null;
+        }
+    }
+}
