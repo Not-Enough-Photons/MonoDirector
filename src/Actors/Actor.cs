77e0b062576f6b5ac4a54e9ebbd620fa8633fa84
adamdev <hmullinsk@hotmail.com>
2023-07-26 00:11:07 -0400

    Reduced frames on the root bone

--- src/Actors/Actor.cs
+++ src/Actors/Actor.cs
@@ -175,9 +175,12 @@ namespace NEP.MonoDirector.Actors
                 Quaternion previousRotation = previousTransformFrames[i].rotation;
                 Quaternion nextRotation = nextTransformFrames[i].rotation;
 
-                clonedRigBones[i].position = Vector3.Lerp(previousPosition, nextPosition, delta);
+                if(i == 0)
+                {
+                    clonedRigBones[i].position = Vector3.Lerp(previousPosition, nextPosition, delta);
+                }
+
                 clonedRigBones[i].rotation = Quaternion.Slerp(previousRotation, nextRotation, delta);
-                
 #if DEBUG
                 previousFrameDebugger[i].position = previousPosition;
                 previousFrameDebugger[i].rotation = previousRotation;
@@ -302,7 +305,7 @@ namespace NEP.MonoDirector.Actors
                     tempFrames[i] = new ObjectFrame(default, default);
                     continue;
                 }
-                
+
                 Vector3 bonePosition = boneList[i].position;
                 Quaternion boneRotation = boneList[i].rotation;
 
