e5635fd7afc8515eac0bf65b0a2eb9b7e20598d7
adamdev <adamdevofficial@protonmail.com>
2026-05-09 04:14:13 -0400

    Added TransformExtensions with a dedicated LookAtYAxis method

---
+++ src/Extensions/TransformExtensions.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace NEP.MonoDirector.Extensions;
+
+public static class TransformExtensions
+{
+    public static void LookAtYAxis(this Transform transform, Transform target, float offset = 0f)
+    {
+        transform.LookAtYAxis(target.position, offset);
+    }
+
+    public static void LookAtYAxis(this Transform transform, Vector3 target, float offset = 0f)
+    {
+        Vector3 lookRotation = Quaternion.LookRotation(target - transform.position).eulerAngles;
+        Quaternion yRotation = Quaternion.Euler(0f, lookRotation.y + offset, 0f);
+        transform.rotation = yRotation;
+    }
+}
